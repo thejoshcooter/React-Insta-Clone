13968efecb2f20e5fb00dd9ec7811189b8f566c2
josh cooter <thejoshcooter@gmail.com>
2019-04-30 11:23:42 -0500

    add container, content container, sidebar containers

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <SearchBar />
+      <div className="container">
+        <div className="content-container">
+
+        </div>
+
+        <div className="sidebar-container">
+
+        </div>
+      </div>
     </div>
   );
 }
