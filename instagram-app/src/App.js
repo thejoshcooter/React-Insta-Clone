b599994c20fa75bad8f1fab3cfcf92bdb75ee84f
josh cooter <thejoshcooter@gmail.com>
2019-04-30 11:46:00 -0500

    import dummy data

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import dummyData from './dummy-data'
 import SearchBar from './components/SearchBar/SearchBar'
 import PostContainer from './components/PostContainer/PostContainer'
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      posts: dummyData
+    }
   };
 
   render() {
