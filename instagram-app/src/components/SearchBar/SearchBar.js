b9f3a3addf0e1f43df7dc5342de061bd8b2ef651
josh cooter <thejoshcooter@gmail.com>
2019-04-30 11:15:49 -0500

    add basic Searchbar component

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -3,9 +3,24 @@ import './SearchBar.css'
 
 const SearchBar = props => {
     return (
-        <div>
-            <h1>SearchBar Here!</h1>
-        </div>
+        <nav className="main-nav">
+            
+                <div className="logo">
+                    <i className="fab fa-instagram nav-icon" />
+                    <img src="https://www.bing.com/th?id=OIP.7MNhbuSUbRqr6V_w_korXQAAAA&pid=Api&rs=1&p=0" alt="Instagram Logo" />
+                </div>
+
+                <div className="search">
+                    <textarea type="text" placeholder="search" />
+                </div>
+
+                <div className="menu">
+                    <i className="far fa-compass nav-icon" />
+                    <i className="fas fa-heart nav-icon" />
+                    <i className="fas fa-user nav-icon" />
+                </div>
+            
+        </nav>
     );
 };
 
