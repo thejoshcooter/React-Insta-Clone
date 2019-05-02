8b2bf26ce52b546e4a023c03ca3c964894550bbc
josh cooter <thejoshcooter@gmail.com>
2019-05-01 22:25:16 -0500

    add post date. add comment box. add moment.js library.

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CommentSection.css'
+import moment from 'moment';
 
 const CommentSection = props => {
     return (
@@ -14,6 +15,21 @@ const CommentSection = props => {
             {props.post.comments.map(index => {
                 return <div className="comment"><span>{index.username}</span><p>{index.text}</p></div>
             })}
+
+            <span className="timestamp">{props.post.timestamp}</span>
+
+            <CommentField />
+        </div>
+    );
+};
+
+
+
+const CommentField = () => {
+    return (
+        <div className="comment-field">
+            <textarea type="text" placeholder="add a comment..." />
+            <button>Post</button>
         </div>
     );
 };
